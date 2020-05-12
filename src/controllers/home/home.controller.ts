60ed6a4db8d239cdc35c116ec75470f0a694d95a
laxmikantphadke@gmail.com <laxmikantphadke@gmail.com>
2020-05-12 18:43:37 +0530

    stock

--- src/controllers/home/home.controller.ts
+++ src/controllers/home/home.controller.ts
@@ -4,22 +4,24 @@ import IControllerBase from "interfaces/IControllerBase.interface";
 import axios from "axios";
 
 class HomeController implements IControllerBase {
-
   public router = express.Router();
 
   constructor() {
     this.initRoutes();
-
   }
-  
 
   public initRoutes() {
     this.router.get("/", async (req: Request, res: Response) => {
-
-      const result = await axios.get('https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20100').then(x=>x.data.data.map(x=>x.symbol)).catch(e=>console.log(e))
-      res.send(result)
+      const result = await axios
+        .get(
+          "https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%20100"
+        )
+        .then((x) => x.data.data.map((x) => x.symbol))
+        .catch((e) => {
+          console.log(e);
+        });
+      res.send(result);
     });
-   
   }
 }
 
